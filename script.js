58763bfe5f7dff2d6ae3ba8ea0faf8457061c24f
Whipl4sh <135068233+Whipl4sh@users.noreply.github.com>
2023-05-30 17:42:47 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+function showalert () {alert("Hola,esto es una alerta desde javascript"); }
+document.querySelector("button.button-menu-toggle")
+.addEventListener("click", function() {
+document.querySelector(".nav-links").
+classList.toggle("nav-links-responsive")})
